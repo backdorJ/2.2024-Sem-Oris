ec722cf62f55809f40e94442c1bf146bc5d244cf
Damir Nabiullin <cdbacjdorz@yandex.ru>
2024-02-24 18:59:50 +0300

    Вторая домашняя работа

---
+++ second-lesson-pokemon/src/Component/Wrapper.jsx
@@ -0,0 +1,35 @@
+import './Card'
+import '../css/Wrapper.css'
+import React from "react";
+import Card from "./Card";
+import NotFound from "./NotFound";
+
+const Wrapper = ({pokemonsData, filterPoke, ref}) => {
+    let filteredData = pokemonsData
+        .filter((item) =>
+            item.species.name.includes(filterPoke)
+            || item.id == filterPoke
+            || item.species.name == filterPoke);
+
+    if (filteredData.length === 0)
+        return <NotFound />
+
+    return (
+        <div className="wrapper">
+            {
+                filteredData.map((item, index) => {
+                    {
+                            return <Card
+                                img={item.sprites.front_default}
+                                key={item.id}
+                                name={item.species.name}
+                                id={item.id}
+                                btns={item.types}/>
+                    }
+                })
+            }
+        </div>
+    )
+}
+
+export default Wrapper;
