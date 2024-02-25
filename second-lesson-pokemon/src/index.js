5f9f50184e5ba256f937ab2208f79f6d14c4eda2
Damir Nabiullin <cdbacjdorz@yandex.ru>
2024-02-25 12:20:24 +0300

    fix bugs

--- second-lesson-pokemon/src/index.js
+++ second-lesson-pokemon/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter , Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import CardInfo from "./pages/details/components/CardInfo";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/poke-info/:name" element={<CardInfo />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 );
