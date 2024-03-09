471d30212bf767004dd13838a51147b4bb30a88d
Damir Nabiullin <cdbacjdorz@yandex.ru>
2024-03-09 14:22:37 +0300

    hotfix: Доработан поиск

--- second-lesson-pokemon/src/Component/Wrapper.jsx
+++ second-lesson-pokemon/src/Component/Wrapper.jsx
@@ -6,11 +6,17 @@ import NotFound from "./NotFound";
 
 const Wrapper = ({pokemonsData, filterPoke}) => {
     let filteredData = pokemonsData
-        .filter((item) =>
-            item.species.name.includes(filterPoke)
-            || item.id === filterPoke
-            || item.species.name === filterPoke);
-
+        .filter((item) => {
+            // Добавляем проверку на item, чтобы избежать ошибки при обращении к свойству name
+            if (!item) {
+                return false;
+            }
+            return (
+                item.species.name.toLowerCase().includes(filterPoke.toLowerCase())
+                || item.id === filterPoke
+                || item.species.name === filterPoke
+            );
+        });
 
     if (filteredData.length === 0)
         return <NotFound />
@@ -19,14 +25,19 @@ const Wrapper = ({pokemonsData, filterPoke}) => {
         <div className="wrapper">
             {
                 filteredData.map((item, _) => {
-                    {
-                        return <Card
+                    // Добавляем дополнительную проверку на item, чтобы избежать ошибки при обращении к свойствам item
+                    if (!item) {
+                        return null;
+                    }
+                    return (
+                        <Card
                             img={item.sprites.other.home.front_shiny}
                             key={item.id}
                             name={item.species.name}
                             id={item.id}
-                            btns={item.types}/>
-                    }
+                            btns={item.types}
+                        />
+                    );
                 })
             }
         </div>
