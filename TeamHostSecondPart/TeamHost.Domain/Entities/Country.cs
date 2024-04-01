fb06532fa26758d3cc102b83599ede50ef85acae
Damir Nabiullin <cdbacjdorz@yandex.ru>
2024-04-01 16:35:36 +0300

    TeamHost Second Part (2 Домашняя работа)

---
+++ TeamHostSecondPart/TeamHost.Domain/Entities/Country.cs
@@ -0,0 +1,34 @@
+using TeamHost.Domain.Common;
+
+namespace TeamHost.Domain.Entities;
+
+/// <summary>
+/// Страна
+/// </summary>
+public class Country : BaseAuditableEntity
+{
+    /// <summary>
+    /// Название
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// Числовой код страны
+    /// </summary>
+    public int Code { get; set; }
+
+    /// <summary>
+    /// 2х-буквенный код
+    /// </summary>
+    public string? AplhaTwo { get; set; }
+
+    /// <summary>
+    /// 3х-буквенный код
+    /// </summary>
+    public string? AplhaThree { get; set; }
+
+    /// <summary>
+    /// Компании
+    /// </summary>
+    public ICollection<Developer> Developers { get; set; } = new List<Developer>();
+}
---
+++ TeamHostSecondPart/TeamHost.Persistence/Context/EfContext.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using TeamHost.Application.Interfaces;
+using TeamHost.Domain.Entities;
+using TeamHost.Persistence.Configurations;
+
+namespace TeamHost.Persistence.Context;
+
+public class EfContext : DbContext, IDbContext
+{
+    public EfContext(DbContextOptions<EfContext> options)
+        : base(options)
+    {
+    }
+
+    /// <inheritdoc />
+    public DbSet<Category> Categories { get; set; }
+
+    /// <inheritdoc />
+    public DbSet<Country> Countries { get; set; }
+
+    /// <inheritdoc />
+    public DbSet<Developer> Developers { get; set; }
+
+    /// <inheritdoc />
+    public DbSet<Game> Games { get; set; }
+
+    /// <inheritdoc />
+    public DbSet<MediaFile> MediaFiles { get; set; }
+
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new CountryConfiguration());
+        modelBuilder.ApplyConfiguration(new DeveloperConfiguration());
+        modelBuilder.ApplyConfiguration(new GameConfiguration());
+        modelBuilder.ApplyConfiguration(new MediaFileConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
